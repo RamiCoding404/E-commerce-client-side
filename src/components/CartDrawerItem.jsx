024b4b663635c82f938b37040075fe8a38e78cd7
RamiCoding404 <ramiibrahim404@gmail.com>
2024-02-01 23:24:59 +0200

    bug_image_cart

--- src/components/CartDrawerItem.jsx
+++ src/components/CartDrawerItem.jsx
@@ -15,9 +15,7 @@ const CartDrawerItem = ({
     <>
       <Flex alignItems={"center"} mb={3} py={2}>
         <Image
-          src={`${import.meta.env.VITE_SERVER_URL}${
-            thumbnail.data.attributes.url
-          }`}
+          src={thumbnail.data.attributes.url}
           alt={title}
           w={"60px"}
           h={"60px"}
